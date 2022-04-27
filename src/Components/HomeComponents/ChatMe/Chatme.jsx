b08dc08c137730f457e141dd9a0c12d0894a29a9
C100-133 <c100-133@C100-133s-iMac.local>
2022-04-27 18:19:20 +0530

    update

--- src/Components/HomeComponents/ChatMe/Chatme.jsx
+++ src/Components/HomeComponents/ChatMe/Chatme.jsx
@@ -6,32 +6,13 @@ import { Friends } from './Friends';
 import { FriendsHangOut } from './FriendsHangOut/FriendsHangOut';
 
 export const Chatme = () => {
-
     const { user } = useContext(UserContext);
     const { id } = useParams();
 
-    // return (
-    //     <div className={style.initial}>
-    //         <div>
-    //             <h1>{`Hey, ${user.name}`}</h1>
-    //             <p>Select server & Have a fun 😃 </p>
-    //         </div>
-    //     </div>
-    // )
-
-    if (id !== undefined || !isNaN(id)) {
-        return (
-            <section className={style.chatme_section}>
-                <Friends user={user} />
-                <Outlet />
-            </section>
-        )
-    } else {
-        return (
-            <section className={style.chatme_section}>
-                <Friends user={user} />
-                <FriendsHangOut />
-            </section>
-        )
-    }
+    return (
+        <section className={style.chatme_section}>
+            <Friends user={user} />
+            {(id !== undefined || !isNaN(id)) ? <Outlet /> : <FriendsHangOut />}
+        </section>
+    )
 }
