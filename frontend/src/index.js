1c401c9ae1c4f796c3b05a84f6d2058f1616a143
ykp-narola <ykp@narola.email>
2024-09-03 18:58:31 +0530

    first commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./Context/auth-context";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { UserContextProvider } from "./Context/user-context";
 
 ReactDOM.render(
-	<BrowserRouter>
+	<Router>
 		<AuthContextProvider>
 			<UserContextProvider>
 				<App />
 			</UserContextProvider>
 		</AuthContextProvider>
-	</BrowserRouter>,
+	</Router>,
 	document.getElementById("root")
 );
